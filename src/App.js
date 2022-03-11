956406608a827fe00e70bad2e383e7e62cf49530
RatniChaima <ratnichayma@gmail.com>
2022-03-11 22:31:18 +0100

    Calculator Logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 
 import "./styles.css"
 
+export const ACTIONS = {
+    ADD_DIGIT: "add-digit",
+    CHOOSE_OPERATION: "choose-operation",
+    CLEAR: "clear",
+    DELETE_DIGIT: "delete-digit",
+    EVALUATE: "evaluate",
+  }
+
+  function reducer(state, { type, payload }) {
+    switch (type) {
+      case ACTIONS.ADD_DIGIT:
+          return {
+            ...state,
+            currentOperand: `${currentOperand}${payload.digit}`
+          }
+        }
+  }
+
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">221231131</div>
-        <div className="current-operand">51545454</div>
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
