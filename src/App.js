19f1175b34197b8d9277c370a99f3df82a6c18c5
RatniChaima <ratnichayma@gmail.com>
2022-03-12 17:01:16 +0100

    Calculator Logic

--- src/App.js
+++ src/App.js
@@ -27,10 +27,55 @@ export const ACTIONS = {
             ...state, 
             currentOperand:`${state.currentOperand || ""}${payload.digit}`,
           }
+      case ACTIONS.CHOOSE_OPERATION:
+          if (state.currentOperand == null && state.previousOperand == null){
+            return state
+          }
+          if (state.previousOperand == null) {
+              return {
+                  ...state,
+                  operation: payload.operation,
+                  previousOperand: state.currentOperand,
+                  currentOperand: null,
+              }
+          }
+
+          return {
+              ...state,
+              previousOperand: evaluate(state),
+              operation: payload.operation,
+              currentOperand: null
+          }
       case ACTIONS.CLEAR:
           {}
+      case ACTIONS.CLEAR:
+          {}
+      case ACTIONS.CLEAR:
+          return {}
 
         }
+  }
+  function evaluate({ currentOperand, previousOperand, operation }) {
+    const prev = parseFloat(previousOperand)
+    const current = parseFloat(currentOperand)
+    if (isNaN(prev) || isNaN(current)) return ""
+    let computation = ""
+    switch (operation) {
+      case "+":
+        computation = prev + current
+        break
+      case "-":
+        computation = prev - current
+        break
+      case "*":
+        computation = prev * current
+        break
+      case "÷":
+        computation = prev / current
+        break
+    }
+  
+    return computation.toString()
   }
 
 function App() {
